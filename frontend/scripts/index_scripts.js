fd6bc43f66d485ab097adb2698ad797943f42bed
Joshua Jacobs-Rebhun <joshua.jacobs.rebhun94@gmail.com>
2022-06-20 18:25:39 -0400

    Got the table working and styled. Expense form is now popup menu. Still need to further style the form and look into flexbox and grid for potential styling. Also add X button to each row and possibly edit button to table. After that work on localstorage or sessionstorage and then get backend and database working.

--- frontend/scripts/index_scripts.js
+++ frontend/scripts/index_scripts.js
@@ -1,22 +1,80 @@
 
+
+/*
+ * This is the callback function for the form submission event. It gets
+ * the data from the form, creates a new row in the table, adds the data
+ * to the row, and resets the form.
+ */
 function addexpense(event)
 {
+    // stop default form submission
     event.preventDefault();
+
+    // get the data from the form
     let expense_name = document.querySelector('#expense-name').value;
     let expense_date = document.querySelector("#date").value;
     let expense_amount = document.querySelector("#amount").value;
 
-    const list_item = document.createElement("li");
-    const text = expense_name + ": " + expense_date + "\t" + expense_amount.toString();
-    const text_content = document.createTextNode(text);
-    list_item.appendChild(text_content);
+    // create new row for table
+    const table_row = document.createElement("tr");
 
-    let expense_list = document.querySelector("#display-list");
-    expense_list.appendChild(list_item);
+    // create new columns for new table element
+    const table_data_name = document.createElement("td");
+    const table_data_date = document.createElement("td");
+    const table_data_amount = document.createElement("td");
 
+    // create text content for each column
+    const table_name_text = document.createTextNode(expense_name);
+    const table_date_text = document.createTextNode(expense_date);
+    const table_amount_text = document.createTextNode(expense_amount.toString());
+
+    // add text to table columns
+    table_data_name.appendChild(table_name_text);
+    table_data_date.appendChild(table_date_text);
+    table_data_amount.appendChild(table_amount_text);
+
+    // add table columns to new table row
+    table_row.appendChild(table_data_name);
+    table_row.appendChild(table_data_date);
+    table_row.appendChild(table_data_amount);
+
+    // add row to table
+    let expense_table = document.querySelector('#expense-table');
+    expense_table.appendChild(table_row);
+
+    // reset the form
     let input_form = document.querySelector('form');
     input_form.reset();
+
+    let form_div = document.querySelector('#expense-form');
+    form_div.style.display = "none";
 }
 
 
-document.querySelector('form').addEventListener('submit', addexpense)
+/*
+ * Callback function for when the 'add expense' button is clicked.
+ * Displays the expense form.
+ */
+function display_form(event)
+{
+    let input_form = document.querySelector('#expense-form');
+    input_form.style.display = "block";
+    input_form.style.position = "absolute";
+    input_form.style.zIndex = "10";
+}
+
+
+/*
+ * Callback function for X button click. Hides the form.
+ */
+function hide_form(event)
+{
+    let input_form = document.querySelector('#expense-form');
+    input_form.style.display = "none";
+}
+
+
+
+document.querySelector('form').addEventListener('submit', addexpense);
+document.querySelector('#add-button').addEventListener('click', display_form);
+document.querySelector('#form-exit-button').addEventListener('click', hide_form);
