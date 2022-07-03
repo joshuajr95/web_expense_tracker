5834b175e394d1e598e2a9c24dc63dc28cd3e033
Joshua Jacobs-Rebhun <joshua.jacobs.rebhun94@gmail.com>
2022-07-03 00:09:47 -0400

    Got the form styled and looking nicer. The rows in the table are also better looking and now have edit and remove buttons. Will get the functionality of those working in a little bit.

--- frontend/scripts/index_scripts.js
+++ frontend/scripts/index_scripts.js
@@ -1,5 +1,10 @@
 
 
+// global variables for keeping track of number of table rows
+let numExpenses = 0;
+
+
+
 /*
  * This is the callback function for the form submission event. It gets
  * the data from the form, creates a new row in the table, adds the data
@@ -17,11 +22,14 @@ function addexpense(event)
 
     // create new row for table
     const table_row = document.createElement("tr");
+    table_row.id = "row-" + numExpenses.toString();
 
     // create new columns for new table element
     const table_data_name = document.createElement("td");
     const table_data_date = document.createElement("td");
     const table_data_amount = document.createElement("td");
+    const table_data_edit_button = document.createElement("td");
+    const table_data_remove_button = document.createElement("td");
 
     // create text content for each column
     const table_name_text = document.createTextNode(expense_name);
@@ -33,14 +41,47 @@ function addexpense(event)
     table_data_date.appendChild(table_date_text);
     table_data_amount.appendChild(table_amount_text);
 
+
+    // create edit and remove buttons for each row
+    const data_edit = document.createElement("button");
+    const data_remove = document.createElement("button");
+
+    // create text content for buttons
+    const edit_text = document.createTextNode("Edit");
+    const remove_text = document.createTextNode("Remove");
+
+    // add button text nodes to button elements
+    data_edit.appendChild(edit_text);
+    data_remove.appendChild(remove_text);
+
+    // add class to buttons to allow them to be styled properly
+    data_edit.classList.add("table-button");
+    data_remove.classList.add("table-button");
+
+    data_edit.addEventListener()
+
+    let data_edit_id = "data-edit-row-" + numExpenses.toString();
+    let data_remove_id = "data-remove-row-" + numExpenses.toString();
+    data_edit.id = data_edit_id;
+    data_remove.id = data_remove_id;
+
+    table_data_edit_button.appendChild(data_edit);
+    table_data_remove_button.appendChild(data_remove);
+
     // add table columns to new table row
     table_row.appendChild(table_data_name);
     table_row.appendChild(table_data_date);
     table_row.appendChild(table_data_amount);
 
+    table_row.appendChild(table_data_edit_button);
+    table_row.appendChild(table_data_remove_button);
+
     // add row to table
-    let expense_table = document.querySelector('#expense-table');
+    let expense_table = document.querySelector('#expense-table-body');
     expense_table.appendChild(table_row);
+
+    console.log(numExpenses);
+    numExpenses++;
 
     // reset the form
     let input_form = document.querySelector('form');
@@ -59,7 +100,7 @@ function display_form(event)
 {
     let input_form = document.querySelector('#expense-form');
     input_form.style.display = "block";
-    input_form.style.position = "absolute";
+    //input_form.style.position = "fixed";
     input_form.style.zIndex = "10";
 }
 
